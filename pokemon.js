9e291393ad4a5f5a2404333d88f9574c9c84d1b2
juancamacho-otf <juan.pelaez@onthefuze.com>
2025-04-24 09:56:27 -0500

    funcioanlidad de eleccion de mascota, tanto de jugador como de maquina

--- pokemon.js
+++ pokemon.js
@@ -23,6 +23,28 @@ function selecionPokemon(){
     }else{
         alert('selecciona una mascota😒')
     }
+    selecionPokemonpc()
+}
+/* 
+esta funcion se ejecuta arriba al nosotros elejir el pokemon, es lo mismo de arriba es tomar el id de el span
+y cambiarle el valor que tiene, asi mismo es validando la aleatoridad que tiene un rango de 1 a 3 
+*/
+
+function selecionPokemonpc(){
+    let pokemonAleatoria = aleatorio(1,3)
+    let pokemonEne = document.getElementById('pokemon-enemigo')
+
+    if (pokemonAleatoria == 1) {
+        pokemonEne.innerHTML = 'pikachu'
+    } else if (pokemonAleatoria == 2) {
+        pokemonEne.innerHTML = 'charmander'
+    } else {
+        pokemonEne.innerHTML = 'squirtle'
+    }
+}
+// esta funcion debe tener muy presente porque es la manera de dar un rago a la aleotoridad 
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 //le estamos  diciendo a la ventana del navegador que escuche despues de cargar el html asi no nos va a dar errores 
 window.addEventListener('load', iniciarJuego)  
